99a3865febc2325a3c20dfcba929df742d6873ba
Dell <Dell@DESKTOP-ELUG4U3>
2025-07-09 18:16:09 +0530

    commited for statment by chaitrali

---
+++ GitProgect/src/com/chaitralidemo/PositiveNegativeDemo.java
@@ -0,0 +1,29 @@
+package com.chaitralidemo;
+
+import java.util.Scanner;
+
+public class PositiveNegativeDemo {
+public static void main(String[] args) {
+	ifpositive_negative();
+	ifelsepositive_negative();
+}
+  static void ifpositive_negative() {
+	 Scanner scanner =new Scanner(System.in); 
+	 System.out.println("enter a value");
+	 int value =scanner.nextInt();
+	 if(value > 1)
+	 System.out.println("enter a positive value");
+	 System.out.println("enter a  negative value"); 
+	
+ }
+  static void ifelsepositive_negative() {
+		 Scanner scanner =new Scanner(System.in);
+		 System.out.println("enter a value");
+		 int value =scanner.nextInt(); 
+		 if(value < 1)
+		 System.out.println("enter a positive value");
+		 else
+		 System.out.println("enter a  negative value");
+		  
+	 }
+}
